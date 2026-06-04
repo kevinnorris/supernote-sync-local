79409bd11ac6a48402755ce257f60da3171149d5
kevin <kevin.n0rris@pm.me>
2026-06-05 09:54:02 +1200

    Improved local log view

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,15 @@
 import pino, {type LevelWithSilent} from 'pino';
 
 export function makeLogger(level: LevelWithSilent): pino.Logger {
-	return pino({level});
+	return pino({
+		level,
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				colorize: true,
+				translateTime: 'HH:MM:ss',
+				ignore: 'pid,hostname',
+			},
+		},
+	});
 }
